16bb27f90deb774db178b757191d6719dd6f99e5
Niels Soete <soeteniels1803@gmail.com>
2025-01-17 10:52:46 +0100

    add mouse / monitor scripts

---
+++ monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-1-0 --mode 3440x1440 --rate 100
---
+++ mouse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-prop "SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Profile Enabled" 0 1 0
+
