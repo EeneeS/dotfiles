b7f3a7662e9c2e96e326c0bf9f19eda5011573ca
eenees <soeteniels1803@gmail.com>
2025-05-05 20:18:00 +0200

    work config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,39 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } }
-          }
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } }
+                    }
+                },
+            }
         },
-      }
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup{}
-      lspconfig.ts_ls.setup{}
-      lspconfig.gopls.setup{}
-      lspconfig.pyright.setup{}
-      lspconfig.tailwindcss.setup{}
-    end,
-  }
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            -- Set up the other LSPs you need
+            lspconfig.lua_ls.setup{}
+            lspconfig.tsserver.setup{}
+            lspconfig.pyright.setup{}
+
+            require('lspconfig').angularls.setup {
+                cmd = {"ngserver", "--stdio"},
+                init_options = {
+                    -- Any initialization options needed
+                },
+                root_dir = require('lspconfig').util.root_pattern('angular.json', 'project.json'),
+                on_new_config = function(new_config, new_root_dir)
+                    new_config.cmd_env = {
+                        NODE_PATH = "/home/niels/.nvm/versions/node/v22.15.0/lib/node_modules"
+                    }
+                end,
+            }
+
+        end,
+    }
 }
+
